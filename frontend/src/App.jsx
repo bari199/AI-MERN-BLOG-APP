87f68b1a339788d1fc33b0d72b7f4409c7e96e84
bari@199 <bari1999smab@gmail.com>
2025-11-15 09:40:59 -0800

    added all files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,56 @@
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import {Toaster} from "react-hot-toast";
+import BlogLandingPage from "./pages/Blog/components/BlogLandingPage";
+import BlogPostView from "./pages/Blog/components/BlogPostView";
+import PostByTags from "./pages/Blog/components/PostByTags";
+import SearchPosts from "./pages/Blog/components/SearchPosts";
+import AdminLogin from "./pages/Admin/components/AdminLogin";
+import PrivateRoutes from "./routes/PrivateRoutes";
+import Dashboard from "./pages/Admin/components/Dashboard";
+import BlogPost from "./pages/Admin/components/BlogPost";
+import BlogPostEditor from "./pages/Admin/components/BlogPostEditor";
+import Comments from "./pages/Admin/components/Comments";
+
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<BlogLandingPage />} />
+          <Route path="/:slug" element={<BlogPostView/>} />
+          <Route path="/tag/:tagName" element={<PostByTags/>} />
+          <Route path="/search" element={<SearchPosts/>} />
+
+
+          {/* Admin Routes */}
+          <Route element={<PrivateRoutes allowedRoutes={["admin"]}/>}/>
+            <Route path="/admin/dashboard" element={<Dashboard/>}/>
+            <Route path="/admin/posts" element={<BlogPost/>}/>
+            <Route path="/admin/create" elements={<BlogPostEditor/>}/>
+            <Route path="/admin/edit/:postSlug"
+            element={<BlogPostEditor isEdit={true}/>}/>
+            <Route path="/admin/comments" elements={<Comments/>}/>
+
+
+
+
+
+          <Route path="/admin-login" element={<AdminLogin/>} />
+        </Routes>
+      </Router>
+
+
+      <Toaster
+        toastOptions={{
+          className:"",
+          style:{
+            fontSize:"13px",
+          },
+        }}
+        />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/Cards/CharAvatar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CharAvatar = ({fullName, width, height, style}) => {
+  return (
+    <div>CharAvatar</div>
+  )
+}
+
+export default CharAvatar
